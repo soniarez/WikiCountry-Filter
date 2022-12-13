1bf57b5172ef939c865248614f1b25d7764ba30a
soniarez <101676781+soniarez@users.noreply.github.com>
2022-12-13 01:24:05 -0300

    selection by language dropdown

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -3,16 +3,28 @@ import TableByRegion from '../components/TableByRegion';
 
 const Dashboard = ({ rawCountriesData }) => {
   const [regions, setRegions] = useState<string[]>([]);
-  const [selectedRegion, setSelectedRegion] = useState<string>('Asia');
+  const [selectedRegion, setSelectedRegion] = useState<string>('');
+  const [languages, setLanguages] = useState<string[]>([]);
+  const [selectedLanguage, setSelectedLanguage] = useState<string>('');
 
-  //console.log(rawCountriesData, 'rawCountriesData');
+  // console.log(rawCountriesData, 'rawCountriesData');
+
+  //console.log(rawCountriesData[145].languages[0].name, "languages.name")
+  
 
   useEffect(() => {
     fiterByRegion();
+
   }, []);
 
   const handleRegionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedRegion(event.target.value);
+    fiterByLanguagePerRegion()
+    //console.log(selectedLanguage, " selectedlenguages")
+  };
+
+  const handleLanguageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedLanguage(event.target.value);
   };
 
   const fiterByRegion = () => {
@@ -20,20 +32,47 @@ const Dashboard = ({ rawCountriesData }) => {
       ...new Set(rawCountriesData.map(item => item.region)),
     ];
     setRegions(uniqueRegions);
+
   };
+
+  const fiterByLanguagePerRegion = () => {
+    const filteredData = rawCountriesData.filter(
+      item => item.region === selectedRegion
+    );
+    console.log(filteredData, "filteredData")
+    const uniqueLanguages = [
+      ...new Set(filteredData.map(item => item.languages[0].name)),
+    ];
+    setLanguages(uniqueLanguages);
+  };
+  
+  //console.log(languages)
 
   return (
     <div>
       <h1>Wiki Country</h1>
       {/*Sacar select y select array (map) a componentes independientes c/u */}
       <label htmlFor="region-select">Choose a region:</label>
-      <select name="regions" className="region-select" defaultValue="Asia" onChange={handleRegionChange}>
-        <option value="regions" disabled>--Regions--</option>
+      <select name="regions" className="region-select" defaultValue="" onChange={handleRegionChange}>
+        <option value="regions">--Regions--</option>
         {regions?.map((region) => (
           <option key={region} value={region} >
             {region}
           </option>
         ))}
+      </select>
+
+      {/*Sacar select y select array (map) a componentes independientes c/u */}
+      <label htmlFor="languages-select">Choose a language:</label>
+      <select name="languages" className="language-select" defaultValue="" onChange={handleLanguageChange}>
+        <option value="languages" >--Languages--</option>
+        {languages?.map((language) => (
+          <option key={language} value={language} >
+            {language}
+          </option>
+        ))} 
+
+
       </select>
 
       <TableByRegion rawCountriesData={rawCountriesData} selectedRegion={selectedRegion} />
